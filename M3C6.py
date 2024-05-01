4bfd4ca3c81c0a4b09045fdf35c88f431e800a8c
PaulaLuinaFernandez <152091955+PaulaLuinaFernandez@users.noreply.github.com>
2024-05-01 22:56:43 +0200

    Add files via upload

---
+++ M3C6.py
@@ -0,0 +1,6 @@
+class Usuario:
+    def __init__(self, nombre_usuario, contrasena):
+        self.nombre_usuario = nombre_usuario
+        self.contrasena = contrasena
+
+usuario_uno = Usuario("Paula", "Password")
